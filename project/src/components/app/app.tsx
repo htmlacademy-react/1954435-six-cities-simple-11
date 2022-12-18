56c025829521e44537aa97a3304217e17917c202
Andrew Samohvalov <andrew.samohv@gmail.com>
2022-12-18 15:04:55 +0300

    Подключает MOKI в Index

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -6,12 +6,17 @@ import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScren from '../../pages/login-screen/login-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import {Offers} from '../../types/offer';
+import {Reviews} from '../../types/review';
 
 type AppProps = {
   roomCardCount: number;
+  offers: Offers;
+  reviews: Reviews;
 };
 
-function App({roomCardCount}: AppProps): JSX.Element {
+function App(props: AppProps): JSX.Element {
+  const {roomCardCount, offers, reviews} = props;
   return(
     <HelmetProvider>
       <BrowserRouter>
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './components/app/app';
+import {offers} from './mocks/offers';
+import {reviews} from './mocks/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-const Setting = {
-  PlaceCardCount: 5,
-} as const;
 
 root.render(
   <React.StrictMode>
-    <App roomCardCount={Setting.PlaceCardCount} />
+    <App
+      roomCardCount = {offers.length}
+      offers = {offers}
+      reviews = {reviews}
+    />
   </React.StrictMode>,
 );
